2dbbd6b1f720d8d8fec9b360334118601434d9d6
Ice-Cirno <ice9coffee@gmail.com>
2019-10-02 02:23:57 +0800

    new feature: arena error code: 103

--- priconne/plugins/chat.py
+++ priconne/plugins/chat.py
@@ -5,13 +5,13 @@ from nonebot.permission import *
 async def say_sorry(session: CommandSession):
     await session.send('ごめんなさい！嘤嘤嘤(〒︿〒)')
 
-
-# @on_command('草', only_to_me=False)
-# async def say_kusa(session: CommandSession):
-#     await session.send('草')
-#     group_id = session.ctx['group_id']
-#     user_id = session.ctx['user_id']
-#     await session.bot.set_group_ban(group_id=group_id , user_id=user_id, duration=10*60)
+'''
+@on_command('草', only_to_me=False)
+async def say_kusa(session: CommandSession):
+    await session.send('草')
+    group_id = session.ctx['group_id']
+    user_id = session.ctx['user_id']
+    await session.bot.set_group_ban(group_id=group_id , user_id=user_id, duration=10*60)
 
 
 @on_command('嘤嘤嘤', aliases=('嘤', '嘤嘤', '嘤嘤嘤嘤', '嘤嘤嘤嘤嘤', '嘤嘤嘤嘤嘤嘤',), only_to_me=False)
@@ -26,7 +26,7 @@ async def say_yingyingying(session: CommandSession):
 async def say_ciya(session: CommandSession):
     ciya = MessageSegment.face(13)
     await session.send(ciya)
-
+'''
 
 @on_command('arina-database', aliases=('jjc', 'JJC', 'JJC作业', 'JJC作业网', 'JJC数据库', 'jjc作业', 'jjc作业网', 'pjjc作业网', 'jjc数据库', 'pjjc数据库'))
 async def say_arina_database(session: CommandSession):
