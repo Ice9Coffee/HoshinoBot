94a3be2491bd2d4a65e341fe949217a9ee4305ac
Ice-Cirno <ice9coffee@gmail.com>
2019-10-02 16:54:47 +0800

    小调整
    添加别称; util调整; AUTH_KEY更换; 扭蛋调整

--- priconne/plugins/kokkoro/gacha.py
+++ priconne/plugins/kokkoro/gacha.py
@@ -2,14 +2,19 @@ import random
 
 class Gacha(object):
 
-    up = ['蕾姆']
+    up_prob =  28
+    s3_prob = 100
+    s2_prob = 200
+
+    up = ['蕾姆', '爱蜜莉雅', '跳跃王', '镜华(万圣节)']
 
     # 这里仅填3星常驻角色。不要填UP角否则出率会偏高
     star3 = [
         '杏奈','真步','璃乃','初音','霞','伊绪',
         '咲恋','望','妮诺','秋乃','镜华','智','真琴',
         '伊莉亚','纯','静流','莫妮卡','流夏','吉塔',
-        '亚里莎','安','古蕾娅', '空花(大江户)', '妮诺(大江户)']
+        '亚里莎','安','古蕾娅', '空花(大江户)', '妮诺(大江户)',
+        '华哥', '生菜']
 
     star2 = [
         '茉莉','茜里','宫子','雪','七七香','美里',
@@ -25,7 +30,7 @@ class Gacha(object):
 
 
     @staticmethod
-    def gacha_one(s3_prob:int, s2_prob:int, s1_prob:int, up_prob:int):
+    def gacha_one(up_prob:int, s3_prob:int, s2_prob:int, s1_prob:int=None):
         '''
         sx_prob: x星概率，要求和为1000
         up_prob: UP角色概率（从3星划出）
@@ -37,6 +42,8 @@ class Gacha(object):
         |   ***   |  **  |    *   |
         ---------------------------
         '''
+        if s1_prob is None:
+            s1_prob = 1000 - s3_prob - s2_prob
         total_ = s3_prob + s2_prob + s1_prob
         pick = random.randint(1, total_)
         if pick <= up_prob:
@@ -50,16 +57,18 @@ class Gacha(object):
 
 
     @staticmethod
-    def gacha_10(fes):
+    def gacha_10():
         result = []
         hiishi = 0
-        s3_prob = 50 if fes else 25
-        
+        up = Gacha.up_prob
+        s3 = Gacha.s3_prob
+        s2 = Gacha.s2_prob
+        s1 = 1000 - s3 - s2        
         for _ in range(9):    # 前9连
-            x, y = Gacha.gacha_one(s3_prob, 200, 800-s3_prob, 7)
+            x, y = Gacha.gacha_one(up, s3, s2, s1)
             result.append(x)
             hiishi = hiishi + y
-        x, y = Gacha.gacha_one(s3_prob, 1000-s3_prob, 0, 7)    # 保底第10抽
+        x, y = Gacha.gacha_one(up, s3, s2 + s1, 0)    # 保底第10抽
         result.append(x)
         hiishi = hiishi + y
 
