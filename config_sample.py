d9e90396e8812db86e39cf0d0eff7fda8946f545
Ice-Cirno <ice9coffee@gmail.com>
2019-11-11 16:29:01 +0800

    更新config_sample

--- config_sample.py
+++ config_sample.py
@@ -3,7 +3,17 @@
 
 from nonebot.default_config import *
 
-SUPERUSERS = {10000}   # 填写超级用户的QQ号
-COMMAND_START = {'', '/', '!', '／', '！', '#'} # 命令开始符号（第一个是空字符串）
-HOST = '172.17.0.1'    # 填写CQHTTP的ip，这里是docker的默认配置，如运行在本机，请改为127.0.0.1
-PORT = 8080            # 填写CQHTTP的端口
+SUPERUSERS = [10000]    # 填写超级用户的QQ号
+COMMAND_START = {''}    # 命令开始符号（第一个是空字符串）
+HOST = '127.0.0.1'      # 填写CQHTTP的ip，docker的默认配置为172.17.0.1，WinServer部署则用127.0.0.1
+PORT = 8080             # 填写CQHTTP的端口
+
+# 是否使用Pro版酷Q功能
+IS_CQPRO = False
+
+# 资源库文件夹  Nonebot访问本机资源
+RESOURCE_DIR = '~/.hoshino/res/'
+
+# 资源库 URL  用于docker中的酷Q读取宿主机资源，注意以'/'结尾
+# 若在WinServer上部署则不需本地WebServer，务必留空
+RESOURCE_URL = ''
