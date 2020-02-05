72c85377013e40f85799a241ceac2e395575f687
Ice-Cirno <ice9coffee@gmail.com>
2020-02-05 15:11:45 +0800

    setu设置每日上限

--- hoshino/modules/setu/setu.py
+++ hoshino/modules/setu/setu.py
@@ -1,7 +1,9 @@
 import os
 import re
 import time
+import pytz
 import random
+from datetime import datetime, timedelta
 from collections import defaultdict
 
 from nonebot import on_command, CommandSession, MessageSegment, NoneBot
@@ -13,10 +15,24 @@ from hoshino.service import Service, Privilege
 
 
 sv = Service('setu', manage_priv=Privilege.SUPERUSER, enable_on_default=False)
+_last_setu_day = -1
+_user_setu_count = defaultdict(int)    # {user: gacha_count}
+_max_setu_per_day = 5
+SETU_EXCEED_NOTICE = f'您今天已经冲过{_max_setu_per_day}次了，欢迎明天再来！'
 
 setu_folder = R.img('setu/').path
 last_call_time = defaultdict(int)   # user_id: t in seconds
 cd_time = 5    # in seconds
+
+def check_setu_num(user_id):
+    global _last_setu_day, _user_setu_count
+    now = datetime.now(pytz.timezone('Asia/Shanghai'))
+    day = (now - timedelta(hours=5)).day
+    if day != _last_setu_day:
+        _last_setu_day = day
+        _user_setu_count.clear()
+    return bool(_user_setu_count[user_id] < _max_setu_per_day)
+
 
 def get_setu():
     try:
@@ -36,17 +52,24 @@ def get_setu():
         return MessageSegment.text('Error: 挑选涩图时发生异常')
 
 
-@sv.on_rex(re.compile(r'不够[涩瑟色]|[涩瑟色]图|来一?[点份张].*[涩瑟色]|看过了|铜'), 'group')
+@sv.on_rex(re.compile(r'不够[涩瑟色]|[涩瑟色]图|来一?[点份张].*[涩瑟色]|再来[点份张]|看过了|铜'), 'group')
 async def setu(bot:NoneBot, ctx, match):
-    """
-    随机叫一份涩图，对每个用户有冷却时间
-    """
+    """随机叫一份涩图，对每个用户有冷却时间"""
+
+    uid = ctx['user_id']
+    if not check_setu_num(uid):
+        await bot.send(SETU_EXCEED_NOTICE, at_sender=True)
+        return
+
     now = time.time()
-    if now > (last_call_time[ctx['user_id']] + cd_time):
-        last_call_time[ctx['user_id']] = now
-        try:
-            await bot.send(ctx, get_setu())
-        except CQHttpError:
-            await bot.send(ctx, '涩图太涩，发不出去勒...')
-    else:
+    if now < (last_call_time[uid] + cd_time):
         await bot.send(ctx, '您冲得太快了，请稍候再冲', at_sender=True)
+        return
+
+    # conditions all ok, send a setu.
+    last_call_time[uid] = now
+    _user_setu_count[uid] += 1
+    try:
+        await bot.send(ctx, get_setu())
+    except CQHttpError:
+        await bot.send(ctx, '涩图太涩，发不出去勒...')
