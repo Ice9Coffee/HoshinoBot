84287b3e90dd752957028565051916b93d0b41d2
Ice-Cirno <ice9coffee@gmail.com>
2020-02-07 20:51:21 +0800

    pcr别称更新

--- hoshino/modules/botmanage/data_clean.py
+++ hoshino/modules/botmanage/data_clean.py
@@ -6,11 +6,12 @@ from hoshino.log import logger
 
 @nonebot.scheduler.scheduled_job('cron', hour='20', misfire_grace_time=10, coalesce=True)     #UTC+8 0400
 async def image_cleaner():
-    await nonebot.get_bot().clean_data_dir(data_dir='image')
+    self_id = nonebot.get_bot()._connected_ws_reverse_api_clients.keys()[0]
+    await nonebot.get_bot().clean_data_dir(self_id=self_id, data_dir='image')
     logger.info('Image 文件夹已清理')
 
 
 @on_command('清理数据', permission=perm.SUPERUSER, only_to_me=True)
 async def data_clean(session:CommandSession):
-    await nonebot.get_bot().clean_data_dir(data_dir='image')
+    await nonebot.get_bot().clean_data_dir(self_id=session.ctx['self_id'], data_dir='image')
     await session.send('Image 文件夹已清理')
