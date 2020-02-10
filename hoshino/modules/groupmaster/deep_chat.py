fdd8b855047c47eac21427c1e72e030c1fa030a1
Ice-Cirno <ice9coffee@gmail.com>
2020-02-11 00:11:45 +0800

    新功能：deep_chat

---
+++ hoshino/modules/groupmaster/deep_chat.py
@@ -0,0 +1,26 @@
+import random
+
+from hoshino import aiorequests
+from nonebot import NoneBot
+from hoshino.service import Service, Privilege
+
+sv = Service('deepchat', manage_priv=Privilege.SUPERUSER, enable_on_default=False)
+
+api = 'http://127.0.0.1:7777/message'
+
+@sv.on_message('group')
+async def deepchat(bot:NoneBot, ctx):
+    msg = ctx['message'].extract_plain_text()
+    if random.random() > 0.050 or not msg:
+        return
+    payload = {
+        "msg": msg,
+        "group": ctx['group_id'],
+        "qq": ctx['user_id']
+    }
+    sv.logger.info(payload)
+    rsp = await aiorequests.post(api, data=payload)
+    j = await rsp.json()
+    sv.logger.info(j)
+    if j['msg']:
+        await bot.send(ctx, j['msg'])
