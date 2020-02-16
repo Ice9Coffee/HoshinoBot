f7624454e32a9ee40a04278e94f4cdea2c57ed79
Ice-Cirno <ice9coffee@gmail.com>
2020-02-16 21:11:41 +0800

    月常远征提醒

--- hoshino/modules/kancolle/reminder.py
+++ hoshino/modules/kancolle/reminder.py
@@ -10,9 +10,10 @@ from hoshino.service import Service
 
 
 sv = Service('kc-reminder', enable_on_default=False)
+utc_p8 = pytz.timezone('Asia/Shanghai')
 
-# UTC+8 1400 = UTC+9 1500 = UTC+0 0600 再提前半小时提醒
-@sv.scheduled_job('cron', hour='5', minute='30', second='0', misfire_grace_time=120, coalesce=True) 
+
+@sv.scheduled_job('cron', hour='13', minute='30', misfire_grace_time=120, coalesce=True, timezone=utc_p8)
 async def enshu_reminder(group_list):
     msg = '演习即将刷新！莫让下午的自己埋怨上午的自己：\n「演習」で練度向上！0/3'
     bot = sv.bot
@@ -25,8 +26,7 @@ async def enshu_reminder(group_list):
             sv.logger.error(f'群{group} 投递enshu_reminder失败 {type(e)}')
 
 
-# UTC+8 2302 = UTC+0 1502
-# @sv.scheduled_job('cron', hour='15', minute='2', second='0', misfire_grace_time=120, coalesce=True) 
+# @sv.scheduled_job('cron', hour='23', minute='2', misfire_grace_time=120, coalesce=True, timezone=utc_p8)
 async def setsubun_reminder(group_list):
     msg = '每日自省：[節分任務]節分演習！2DDor2DE演习S胜3次 完成了吗？'
     bot = sv.bot
@@ -37,3 +37,18 @@ async def setsubun_reminder(group_list):
             sv.logger.info(f'群{group} 投递setsubun_reminder成功')
         except nonebot.CQHttpError as e:
             sv.logger.error(f'群{group} 投递setsubun_reminder失败 {type(e)}')
+
+
+@sv.scheduled_job('cron', day='12-14', hour='22', misfire_grace_time=120, coalesce=True, timezone=utc_p8)
+async def ensei_reminder(group_list):
+    now = datetime.now(utc_p8)
+    remain_days = 15 - now.day
+    msg = f'【远征提醒小助手】月常远征还有{remain_days}天刷新！'
+    bot = sv.bot
+    for group, sid in group_list.items():
+        try:
+            await bot.send_group_msg(self_id=random.choice(sid), group_id=group, message=msg)
+            await bot.send_group_msg(self_id=random.choice(sid), group_id=group, message=f'[CQ:at,qq=all]')
+            sv.logger.info(f'群{group} 投递ensei_reminder成功')
+        except nonebot.CQHttpError as e:
+            sv.logger.error(f'群{group} 投递ensei_reminder失败 {type(e)}')
