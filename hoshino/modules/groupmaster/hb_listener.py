e7692f6df6d65d35659a7f851fa961d90858853e
Ice-Cirno <ice9coffee@gmail.com>
2020-02-29 22:28:09 +0800

    反滥用升级

--- hoshino/modules/groupmaster/hb_listener.py
+++ hoshino/modules/groupmaster/hb_listener.py
@@ -13,8 +13,9 @@ bot = nonebot.get_bot()
 BLANK_MESSAGE = Message(MessageSegment.text(''))
 
 @message_preprocessor
-async def black_list(bot, ctx):
-    if ctx['message_type'] == 'group' and Service.check_block_group(ctx['group_id']):
+async def black_filter(bot, ctx):
+    if ctx['message_type'] == 'group' and Service.check_block_group(ctx['group_id']) \
+       or Service.check_block_user(ctx['user_id']):
         ctx['message'] = BLANK_MESSAGE
 
 
@@ -28,13 +29,15 @@ def _check_hbtitle_is_cmd(ctx, title):
 
 @bot.on_message('group')
 async def hb_handler(ctx):
+    user_id = ctx['user_id']
     group_id = ctx['group_id']
     first_msg_seg = ctx['message'][0]
     if first_msg_seg.type == 'hb':
         title = first_msg_seg['data']['title']
         if _check_hbtitle_is_cmd(ctx, title):
             Service.set_block_group(group_id, timedelta(hours=1))
+            Service.set_block_user(user_id, timedelta(hours=24))
             util.silence(ctx, 7 * 24 * 60 * 60)
             msg_from = f"{ctx['user_id']}@[群:{ctx['group_id']}]"
             logger.critical(f'Self: {ctx["self_id"]}, Message {ctx["message_id"]} from {msg_from} detected as abuse: {ctx["message"]}')
-            await bot.send(ctx, "检测到滥用行为，您的操作已被记录。\nbot拒绝响应本群消息1小时", at_sender=True)
+            await bot.send(ctx, "检测到滥用行为，您的操作已被记录并加入黑名单24小时。\nbot拒绝响应本群消息1小时", at_sender=True)
