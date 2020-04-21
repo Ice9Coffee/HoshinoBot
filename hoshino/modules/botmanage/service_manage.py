195de5e2ee5aba4e745256caa81863f92d9cdc84
Ice-Cirno <ice9coffee@gmail.com>
2020-04-21 20:36:20 +0800

    lssv对SUPERUSER增加--group参数

--- hoshino/modules/botmanage/service_manage.py
+++ hoshino/modules/botmanage/service_manage.py
@@ -3,16 +3,28 @@ from functools import cmp_to_key
 from nonebot import on_command, CommandSession
 from nonebot import permission as perm
 from nonebot import CQHttpError
+from nonebot.argparse import ArgumentParser
 
 from hoshino.service import Service, Privilege as Priv
 
 PRIV_NOTE = f'群主={Priv.OWNER} 群管={Priv.ADMIN} 群员={Priv.NORMAL} bot维护组={Priv.SUPERUSER}'
 
-@on_command('lssv', aliases=('服务列表', '功能列表'), permission=perm.GROUP_ADMIN, only_to_me=False)
+@on_command('lssv', aliases=('服务列表', '功能列表'), permission=perm.GROUP_ADMIN, only_to_me=False, shell_like=True)
 async def lssv(session:CommandSession):
-    verbose_all = session.current_arg_text == '-a' or session.current_arg_text == '--all'
-    gid = session.ctx['group_id']
-    msg = ["服务一览："]
+    parser = ArgumentParser(session=session)
+    parser.add_argument('-a', '--all', action='store_true')
+    parser.add_argument('-g', '--group', type=int, default=0)
+    args = parser.parse_args(session.argv)
+    
+    verbose_all = args.all
+    if session.ctx['user_id'] in session.bot.config.SUPERUSERS:
+        gid = args.group or session.ctx.get('group_id')
+        if not gid:
+            session.finish('Usage: -g|--group <group_id> [-a|--all]')
+    else:
+        gid = session.ctx['group_id']
+
+    msg = [f"群{gid}服务一览："]
     svs = Service.get_loaded_services().values()
     svs = map(lambda sv: (sv, sv.check_enabled(gid)), svs)
     key = cmp_to_key(lambda x, y: (y[1] - x[1]) or (-1 if x[0].name < y[0].name else 1 if x[0].name > y[0].name else 0))
