1e36482e22d5031138ed8cc56dc5551e23dfe441
Lancercmd <59906398+Lancercmd@users.noreply.github.com>
2020-04-24 17:12:41 +0800

    Update whois.py
    我宣布咖啡佬牛逼

--- hoshino/modules/priconne/query/whois.py
+++ hoshino/modules/priconne/query/whois.py
@@ -2,15 +2,31 @@ from hoshino.util import FreqLimiter
 from ..chara import Chara
 from . import sv
 
+from nonebot import MessageSegment
+import random
+
 _lmt = FreqLimiter(5)
+
+RAINBOW_P = [
+            f'{str(MessageSegment.at(438971718))}佬牛逼',
+            f'{str(MessageSegment.at(438971718))}佬唯一滴神',
+            f'我宣布{str(MessageSegment.at(438971718))}佬牛逼',
+            ]
 
 @sv.on_rex(r'^[谁誰]是\s*(.{1,20})$', normalize=False)
 async def _whois(bot, ctx, match):
+
     uid = ctx['user_id']
     if not _lmt.check(uid):
         await bot.send(ctx, '您查询得太快了，请稍等一会儿', at_sender=True)
         return
     _lmt.start_cd(uid)
+
+    message = ctx['raw_message'].strip('谁').strip('誰').strip('是')
+
+    if message ==  'Ice-Cirno' or 'Ice咖啡' or '咖啡' or '咖啡佬':
+        await bot.send(ctx, random.choice(RAINBOW_P))
+        return
 
     name = match.group(1)
     chara = Chara.fromname(name)
