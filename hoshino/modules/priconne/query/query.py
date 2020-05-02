b4c0106f0594048bca8be739158352f3c58ed32d
王见思 <5361064+zzbslayer@user.noreply.gitee.com>
2020-05-02 16:45:52 +0800

    Calculate remaining diamond in arena by your rank

--- hoshino/modules/priconne/query/query.py
+++ hoshino/modules/priconne/query/query.py
@@ -85,6 +85,38 @@ async def bcr_sites(session:CommandSession):
     await session.send(BCR_SITES, at_sender=True)
     await util.silence(session.ctx, 60)
 
+@sv.on_command('arena_diamond', aliases=('jjc钻石', '竞技场钻石', 'jjc钻石查询', '竞技场钻石查询'))
+async def arena_diamond(session:CommandSession):
+    rank = int(session.current_arg_text.strip())
+    if (rank > 15000):
+        amount = 42029
+    elif (rank > 12000):
+        amount = (rank / 100 - 120) * 45 + 40679
+    elif (rank > 11900):
+        amount = 40599
+    elif (rank > 7999):
+        amount = (rank / 100 - 80) * 95 + 36799
+    elif (rank > 4000):
+        amount = (rank - 4001) + 32800
+    elif (rank > 2000):
+        amount = (rank - 2001) * 3 + 26800
+    elif (rank > 1000):
+        amount = (rank - 1001) * 5 + 21800
+    elif (rank > 500):
+        amount = (rank - 501) * 7 + 18300
+    elif (rank > 200):
+        amount = (rank - 201) * 13 + 14400
+    elif (rank > 100):
+        amount = (rank - 101) * 35 + 10900
+    elif (rank > 10):
+        amount = (rank - 11) * 60 + 5500
+    elif (rank > 0):
+        amount = (rank - 1) * 550
+    else:
+        amount = 0
+    messages = "剩余钻石：" + str(amount)
+    await session.send(messages, at_sender=True)
+    await util.silence(session.ctx, 60)
 
 YUKARI_SHEET_ALIAS = map(lambda x: ''.join(x), itertools.product(('黄骑', '酒鬼', '黃騎'), ('充电', '充电表', '充能', '充能表')))
 YUKARI_SHEET = f'''
