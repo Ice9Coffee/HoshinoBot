76cde4419a0e91f024a7cbd0ec57a75fe0654ab2
AkiraXie <50363732+AkiraXie@users.noreply.github.com>
2020-05-06 00:23:23 +0800

    feat:add custom

--- hoshino/modules/groupmaster/anti_abuse.py
+++ hoshino/modules/groupmaster/anti_abuse.py
@@ -58,9 +58,9 @@ async def hb_handler(ctx):
 # ============================================ #
 
 BANNED_WORD = (
-    'rbq', 'RBQ', '憨批', '废物', '死妈', '崽种', '傻逼', '傻逼玩意', 
-    '没用东西', '傻B', '傻b', 'SB', 'sb', '煞笔', 'cnm', '爬', 'kkp', 
-    'nmsl', 'D区', '口区', '我是你爹', 'nmbiss', '弱智', '给爷爬', '杂种爬'
+    'rbq', 'RBQ', '憨批', '废物', '死妈', '崽种', '傻逼', '傻逼玩意',
+    '没用东西', '傻B', '傻b', 'SB', 'sb', '煞笔', 'cnm', '爬', 'kkp',
+    'nmsl', 'D区', '口区', '我是你爹', 'nmbiss', '弱智', '给爷爬', '杂种爬','爪巴'
 )
 @on_command('ban_word', aliases=BANNED_WORD, only_to_me=True)
 async def ban_word(session):
