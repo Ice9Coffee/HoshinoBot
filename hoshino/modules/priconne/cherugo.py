5b2bf5e5626bd1cca4b9337e2dcc08e88dffa93e
Ice-Cirno <ice9coffee@gmail.com>
2020-05-07 11:47:32 +0800

    upgrade encoding to gb18030

--- hoshino/modules/priconne/cherugo.py
+++ hoshino/modules/priconne/cherugo.py
@@ -17,7 +17,7 @@ sv = Service('pcr-cherugo')
 
 CHERU_SET = '切卟叮咧哔唎啪啰啵嘭噜噼巴拉蹦铃'
 CHERU_DIC = { c: i for i, c in enumerate(CHERU_SET) }
-ENCODING = 'gbk'
+ENCODING = 'gb18030'
 rex_split = re.compile(r'\b', re.U)
 rex_word = re.compile(r'^\w+$', re.U)
 
@@ -40,7 +40,7 @@ def cheru2word(c:str):
         x = CHERU_DIC.get(b2, 0)
         x = x << 4 | CHERU_DIC.get(b1, 0)
         b.append(x)
-    return bytes(b).decode(ENCODING, 'ignore')
+    return bytes(b).decode(ENCODING, 'replace')
 
 def str2cheru(s:str):
     c = []
@@ -61,14 +61,14 @@ def cheru2str(c:str):
 @sv.on_command('切噜一下')
 async def cherulize(session:CommandSession):
     s = session.current_arg_text
-    if len(s) > 300:
+    if len(s) > 500:
         session.finish('切、切噜太长切不动勒切噜噜...', at_sender=True)
     session.finish('切噜～♪' + str2cheru(s))
 
 @sv.on_rex(r'^切噜～♪', normalize=False)
 async def decherulize(bot, ctx, match):
     s = ctx['plain_text'][4:]
-    if len(s) > 901:
+    if len(s) > 1501:
         await bot.send(ctx, '切、切噜太长切不动勒切噜噜...', at_sender=True)
         return
     msg = '的切噜噜是：\n' + escape(cheru2str(s))
