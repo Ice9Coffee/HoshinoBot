96a4a4f7f998fdb91162b1081aac9dc40a555bc6
Ice-Cirno <ice9coffee@gmail.com>
2020-05-11 01:23:49 +0800

    whois显示最高星级头像

--- hoshino/modules/priconne/query/whois.py
+++ hoshino/modules/priconne/query/whois.py
@@ -5,7 +5,7 @@ from . import sv
 _lmt = FreqLimiter(5)
 
 @sv.on_rex(r'^[谁誰]是\s*(.{1,20})$', normalize=False)
-async def _whois(bot, ctx, match):
+async def whois(bot, ctx, match):
     uid = ctx['user_id']
     if not _lmt.check(uid):
         await bot.send(ctx, '您查询得太快了，请稍等一会儿', at_sender=True)
@@ -13,7 +13,7 @@ async def _whois(bot, ctx, match):
     _lmt.start_cd(uid)
 
     name = match.group(1)
-    chara = Chara.fromname(name)
+    chara = Chara.fromname(name, star=0)
     if chara.id == Chara.UNKNOWN:
         _lmt.start_cd(uid, 600)
         await bot.send(ctx, f'兰德索尔似乎没有叫"{name}"的人\n角色别称补全计划：github.com/Ice-Cirno/HoshinoBot/issues/5\n您的下次查询将于10分钟后可用', at_sender=True)
