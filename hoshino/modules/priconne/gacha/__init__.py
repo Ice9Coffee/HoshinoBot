0e24f4d910cce18aeed46ace8d3e491ce5d25224
Aoba <xuyaqi888@live.cn>
2020-06-05 14:24:46 +0800

    修正因无 CQPRO 导致无法抽一井的错误

--- hoshino/modules/priconne/gacha/__init__.py
+++ hoshino/modules/priconne/gacha/__init__.py
@@ -173,14 +173,18 @@ async def gacha_300(session:CommandSession):
     if lenth <= 0:
         res = "竟...竟然没有3★？！"
     else:
-        step = 4
-        pics = []
-        for i in range(0, lenth, step):
-            j = min(lenth, i + step)
-            pics.append(Chara.gen_team_pic(res[i:j], star_slot_verbose=False))
-        res = concat_pic(pics)
-        res = pic2b64(res)
-        res = MessageSegment.image(res)
+        if sv.bot.config.IS_CQPRO:
+            step = 4
+            pics = []
+            for i in range(0, lenth, step):
+                j = min(lenth, i + step)
+                pics.append(Chara.gen_team_pic(res[i:j], star_slot_verbose=False))
+            res = concat_pic(pics)
+            res = pic2b64(res)
+            res = MessageSegment.image(res)
+        else:
+            res = [f'{c.name}{"★"*c.star}' for c in res]
+            res = ' '.join(res)
 
     msg = [
         f"\n素敵な仲間が増えますよ！ {res}",
