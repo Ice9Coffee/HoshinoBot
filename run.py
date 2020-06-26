905a7671924b5fe0083d9a42633f6d8b13bc5e7c
Ice-Cirno <ice9coffee@gmail.com>
2020-06-26 15:38:17 +0800

    fixed #76

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import hoshino
+import asyncio
 
 bot = hoshino.init()
 app = bot.asgi
 
 if __name__ == '__main__':
-    bot.run(use_reloader=False)
+    bot.run(use_reloader=False, loop=asyncio.get_event_loop())
