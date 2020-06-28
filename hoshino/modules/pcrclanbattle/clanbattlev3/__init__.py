c42903b7b73986f0d63c324f6a0e6aeea1230ffe
Ice-Cirno <ice9coffee@gmail.com>
2020-06-29 00:05:36 +0800

    update README

--- hoshino/modules/pcrclanbattle/clanbattlev3/__init__.py
+++ hoshino/modules/pcrclanbattle/clanbattlev3/__init__.py
@@ -1,5 +1,1 @@
-from hoshino import Service
-
-sv = Service('clanbattlev3', bundle='pcr会战', help_='Hoshino会战管理v3（建设中）')
-
 from .cmdv3 import *
---
+++ hoshino/modules/pcrclanbattle/clanbattlev3/const.py
@@ -0,0 +1,10 @@
+class SERVER:
+    JP = 0x00
+    TW = 0x01
+    CN = 0x02
+
+class CHALLENGE:
+    NORM    = 0x00
+    LAST    = 0x01
+    EXT     = 0x02
+    TIMEOUT = 0x04
