5ad40cc4e500b0ebb059856b6d1bd64f64865435
baimoge <877348613@qq.com>
2020-06-29 15:27:56 +0800

    1. add config

---
+++ hoshino/config/hourcall.py
@@ -0,0 +1,32 @@
+HOUR_CALLS_ON = [
+    "HOUR_CALL_1",
+]
+
+HOUR_CALLS = {
+    "HOUR_CALL_1": [
+        "午夜0点.",
+        "1 o'clock.",
+        "2 o'clock.",
+        "3 o'clock.",
+        "4 o'clock.",
+        "5 o'clock.",
+        "6 o'clock.",
+        "7 o'clock.",
+        "8 o'clock.",
+        "9 o'clock.",
+        "10 o'clock.",
+        "11 o'clock.",
+        "Noon. 好，该吃午饭了。",
+        "1 o'clock.",
+        "2 o'clock.",
+        "3 o'clock.",
+        "4 o'clock.",
+        "5 o'clock.",
+        "6 o'clock.",
+        "7 o'clock.",
+        "8 o'clock.",
+        "9 o'clock.",
+        "10 o'clock.",
+        "11 o'clock.",
+    ],
+}
