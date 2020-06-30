ebe2283c14a272b71f3c72a698901ea9862f6566
Ice-Cirno <ice9coffee@gmail.com>
2020-06-30 21:09:10 +0800

    data update

--- hoshino/modules/priconne/chara.py
+++ hoshino/modules/priconne/chara.py
@@ -196,10 +196,24 @@ class Chara:
 
 
 
-@sucmd('reload-pcr-chara', force_private=False, aliases=('重载角色花名册', ))
+@sucmd('reload-pcr-chara', force_private=False, aliases=('重载花名册', ))
 async def reload_pcr_chara(session: CommandSession):
     try:
         roster.update()
+        await session.send('ok')
+    except Exception as e:
+        logger.exception(e)
+        await session.send(f'Error: {type(e)}')
+
+
+@sucmd('download-pcr-chara-icon', force_private=False, aliases=('下载角色头像'))
+async def download_pcr_chara_icon(session: CommandSession):
+    try:
+        id_ = roster.get_id(session.current_arg_text.strip())
+        assert id_ != UNKNOWN, '未知角色名'
+        download_chara_icon(id_, 6)
+        download_chara_icon(id_, 3)
+        download_chara_icon(id_, 1)
         await session.send('ok')
     except Exception as e:
         logger.exception(e)
