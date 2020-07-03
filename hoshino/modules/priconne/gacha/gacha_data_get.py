94b475af1d99562ae06222c7f769f851285dc3b6
tngsohack <56592599+kkbllt@users.noreply.github.com>
2020-07-03 09:46:16 +0800

    Update gacha_data_get.py
    更换成新的源，换了下结构

--- hoshino/modules/priconne/gacha/gacha_data_get.py
+++ hoshino/modules/priconne/gacha/gacha_data_get.py
@@ -3,18 +3,38 @@ import os, json, requests
 #=====================================================================
 #diy_gacha.json用于自定义卡池
 #=====================================================================
-class gacha_data(object):#卡池
-	jp = {}
-	cn = {}
-	tw = {}
-	all = {}
-	diy = {}
-_gd = gacha_data()
+class gacha_data(object):
+
+	def __init__(self):
+		self.set_gacha()
+
+	def set_gacha(self):
+		default_gacha = json.load(open('default_gacha.json', 'r'))
+		self.__jp = default_gacha['jp']
+		self.__cn = default_gacha['cn']
+		self.__tw = default_gacha['tw']
+		self.__all = default_gacha['all']
+		if os.path.exists('diy_gacha.json'):
+			diy_gacha = json.load(open('diy_gacha.json', 'r'))
+			self.__diy = diy_gacha	
+
+	#用于调用预存的抽卡列表?
+	def get_jp(self):
+		return self.__jp
+	def get_cn(self):
+		return self.__cn
+	def get_tw(self):
+		return self.__tw
+	def get_all(self):
+		return self.__all
+	def get_diy(self):
+		return self.__diy
+
+
 class get_gacha(object):#检查更新数据
 	
 	def __init__(self):
 		self.check_up()
-		self.set_gacha()
 
 	def check_up(self):
 		if os.path.exists('gacha_ver.json'):
@@ -23,7 +43,8 @@ class get_gacha(object):#检查更新数据
 				loaclversion = int(lvj.get('ver', 0))
 		else:
 			loaclversion = 0
-		load_ver = requests.get('https://lolicon.plus/gacha/gacha_ver.json')#演示用的线上verjson
+
+		load_ver = requests.get('https://api.redive.lolikon.icu/gacha/gacha_ver.json')
 		if load_ver.status_code != 200:
 			return -1
 		get_ver = int(load_ver.json().get('ver', 0))
@@ -31,18 +52,9 @@ class get_gacha(object):#检查更新数据
 			return 0
 		with open('gacha_ver.json','w') as lv :
 			lv.write(load_ver.text)
-		load_gacha = requests.get('https://lolicon.plus/gacha/default_gacha.json')#演示用的线上gachajson
+
+		load_gacha = requests.get('https://api.redive.lolikon.icu/gacha/default_gacha.json')
 		if load_gacha.status_code != 200:
 			return -1
 		with open('default_gacha.json','w') as gc:
 			gc.write(load_gacha.text)
-
-	def set_gacha(self):
-		default_gacha = json.load(open('default_gacha.json', 'r'))
-		_gd.jp = default_gacha['jp']
-		_gd.cn = default_gacha['cn']
-		_gd.tw = default_gacha['tw']
-		_gd.all = default_gacha['all']
-		if os.path.exists('diy_gacha.json'):
-			diy_gacha = json.load(open('diy_gacha.json', 'r'))
-			_gd.diy = diy_gacha
