ca1ec94217dcafb0c607c5e17c1da8bf74f63340
Dihe Chen <52123645+Chendihe4975@users.noreply.github.com>
2020-07-17 17:57:57 +0800

    reload nonebot

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ bot = hoshino.init()
 app = bot.asgi
 
 if __name__ == '__main__':
-    bot.run(use_reloader=False, loop=asyncio.get_event_loop())
+    bot.run(use_reloader=True, loop=asyncio.get_event_loop())
