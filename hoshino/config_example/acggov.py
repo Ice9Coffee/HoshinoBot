09703f8f699bb870e1c3dc3164b7fec934fe9d3c
Nekoer <32485369+Nekoer@users.noreply.github.com>
2020-07-19 20:11:22 +0800

    Create acggov.py
    添加acggov模块

---
+++ hoshino/config_example/acggov.py
@@ -0,0 +1,7 @@
+# API_KEY 申请地址https://www.acg-gov.com/account
+ACG_GOV_API_KEY = "APIKEY"
+ACG_GOV_AMAZING_PIC_URL = "https://api.pixiv.hcyacg.com/public/setu"
+
+# 必须是酷Q pro/data/image这个路径，最后不能有斜杠 例如：E:/酷Q Pro/data/image
+ACG_GOV_IMG_PATH = ""
+ACG_GOV_PIC_ORIGIN = True
