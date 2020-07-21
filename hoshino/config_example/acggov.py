bb5b1b01fb44c311d689597622d6a37ecfe27bf5
Nekoer <32485369+Nekoer@users.noreply.github.com>
2020-07-22 00:00:47 +0800

    Update acggov.py

--- hoshino/config_example/acggov.py
+++ hoshino/config_example/acggov.py
@@ -1,6 +1,6 @@
 # API_KEY 申请地址https://www.acg-gov.com/account
 ACG_GOV_API_KEY = "APIKEY"
-ACG_GOV_AMAZING_PIC_URL = "https://api.pixiv.hcyacg.com/public/setu"
+ACG_GOV_AMAZING_PIC_URL = "https://api.acg-gov.com/public/setu"
 
 # 必须是酷Q pro/data/image这个路径，最后不能有斜杠 例如：E:/酷Q Pro/data/image
 ACG_GOV_IMG_PATH = ""
