a5e0d743cb1c0909e70691bea5bb58d02d8d50ee
Ice-Cirno <438971718@qq.com>
2020-10-21 21:24:35 +0800

    remove some holo keyword 部分关键词易误触

--- hoshino/modules/groupmaster/anti_holo.py
+++ hoshino/modules/groupmaster/anti_holo.py
@@ -47,13 +47,13 @@
 # (全文完)
 
 SB_HOLO = '''
-Holo ホロ 木口 术口
-时乃空 ときのそら Tokino Sora 空妈
+Hololive ホロ 木口 术口
+时乃空 ときのそら Tokino 空妈
 萝卜子 ロボ子さん Roboko 大根子
 樱巫女 さくらみこ Sakura Miko 樱火龙 elite工厂厂长 黄油精英巫女 赌鬼巫女 樱污女
 星街彗星 星街 すいせい Hoshimati Suisei 星姐 阿星 彗彗 suisui 有点神经病的蓝发大姐姐 有点大姐姐的蓝发神经病
 夜空梅露 夜空メル Yozora Mel 梅露 梅球王 Banpire 清楚系天才美少女吸血鬼
-夏色祭 夏色まつり Natsuiro Matsuri 马自立 祭妹 夏哥 夏半首
+夏色祭 まつり Natsuiro Matsuri 马自立 祭妹 夏哥 夏半首
 赤井心 赤井はあと Akai Haato 心心 心大人 はぁちゃま 哈恰玛 哈恰嘛
 亚绮 罗森塔尔 アキ ローゼンタール Aki Rosenthal akirose 李姐 力速双A弱精灵
 白上吹雪 白上フブキ Shirakami Fubuki 🌽 fbk 小狐狸 屑狐狸 喵喵狐 debuki 玉米人 赫鲁晓狐 工具狐
@@ -72,7 +72,7 @@ Holo ホロ 木口 术口
 白银诺艾尔 白銀ノエル Shirogane Noel 
 宝钟 玛琳 宝鐘マリン Houshou Marine 无船承运人 山贼
 天音彼方 天音かなた Amane Kanata 彼方碳 かなたん PP天使 天妈 音妹 天音 汤达人 天音腾格尔
-桐生 可可 ココ Kiryū Kiryuu Coco 🐉 蝗 虫皇 龙皇 憨憨龙 ass龙 宝批龙 西成女王
+桐生可可 桐生 ココ Kiryū Kiryuu Coco 🐉 蝗 虫皇 龙皇 憨憨龙 ass龙 宝批龙 西成女王
 角卷绵芽 角巻わため Tsunomaki Watame wtm 毛球 顶顶羊 咚咚羊 畜生羊 木口锤石
 常暗永远 常闇トワ Tokoyami Towa 永远大人 トワ様
 姬森璐娜 姫森 ルーナ Himemori Luuna 璐娜 英语教师
@@ -98,6 +98,7 @@ Airani Iofifteen
 华生 阿米莉亚 Watson Amelia ワトソン アメリア 艾米
 友人A YuujinA
 谷乡元昭 YAGOO 谷郷元昭 Tanigou Motoaki
+斯哈斯哈
 '''.split()
 # 复制完了 快吐了
 
@@ -110,7 +111,7 @@ sv = Service('anti-holo', manage_priv=priv.SUPERUSER)
 
 @sv.on_keyword(SB_HOLO)
 async def anti_holo(bot: HoshinoBot, ev: CQEvent):
-    priv.set_block_user(ev.user_id, timedelta(minutes=5))
-    await util.silence(ev, 5 * 60, skip_su=False)
+    priv.set_block_user(ev.user_id, timedelta(minutes=1))
+    await util.silence(ev, 60, skip_su=False)
     await bot.send(ev, HAHAHA_VTB_TIANGOU.cqcode)
     await bot.delete_msg(self_id=ev.self_id, message_id=ev.message_id)
