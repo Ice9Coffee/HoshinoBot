ac2631e6b23b0013114781e4903fbaf11da62880
Ice-Cirno <438971718@qq.com>
2020-12-23 15:18:25 +0800

    data update & clanbattle stage 5

--- hoshino/modules/twitter/twitter.py
+++ hoshino/modules/twitter/twitter.py
@@ -23,7 +23,7 @@ subr_dic = {
     Service('pcr-twitter', enable_on_default=True, help_='日服Twitter转发', bundle='pcr订阅'): ['priconne_redive', 'priconne_anime'],
     Service('pripri-twitter', enable_on_default=False, visible=False): ['pripri_anime'],
     Service('coffee-favorite-twitter', manage_priv=priv.SUPERUSER,
-            enable_on_default=False, visible=False): ['shiratamacaron', 'k_yuizaki', 'suzukitoto0323', 'watanohara2'],
+            enable_on_default=False, visible=False): ['shiratamacaron', 'k_yuizaki', 'suzukitoto0323'],
 }
 
 latest_info = {}      # { account: {last_tweet_id: int, profile_image: str } }
@@ -127,7 +127,9 @@ async def twitter_poller():
         twts = []
         for account in subr_list:
             twts.extend(buf.get(account, []))
-        await ssv.broadcast(twts, ssv.name, 0.5)
+        if twts:
+            await ssv.broadcast(twts, ssv.name, 0.5)
+
 
 @sv.on_prefix('看推', only_to_me=True)     # for test
 async def one_tweet(bot, ev: CQEvent):
