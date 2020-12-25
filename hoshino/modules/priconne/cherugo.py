a76abc603e5de500d7fcba2997eb3fabc8236a5e
Ice-Cirno <438971718@qq.com>
2020-12-26 02:01:52 +0800

    fix cqcode injection bugs (hoping it is the final one )

--- hoshino/modules/priconne/cherugo.py
+++ hoshino/modules/priconne/cherugo.py
@@ -11,8 +11,6 @@
 import re
 from itertools import zip_longest
 
-from nonebot.message import escape
-
 from hoshino import Service, util
 from hoshino.typing import CQEvent
 
@@ -81,5 +79,5 @@ async def decherulize(bot, ev: CQEvent):
     if len(s) > 1501:
         await bot.send(ev, '切、切噜太长切不动勒切噜噜...', at_sender=True)
         return
-    msg = '的切噜噜是：\n' + util.filt_message(escape(cheru2str(s)))
+    msg = '的切噜噜是：\n' + util.filt_message(util.escape(cheru2str(s)))
     await bot.send(ev, msg, at_sender=True)
