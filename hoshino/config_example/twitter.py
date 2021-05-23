9f547d1cdf0dbca63812f08a218e679503a58c05
Ice-Cirno <438971718@qq.com>
2021-05-24 00:14:39 +0800

    new feature: reload-twitter-stream-daemon

--- hoshino/config_example/twitter.py
+++ hoshino/config_example/twitter.py
@@ -2,3 +2,27 @@ consumer_key = ""
 consumer_secret = ""
 access_token_key = ""
 access_token_secret = ""
+
+follows = {
+    "twitter-stream-test": ["Ice9Coffee"],
+    "kc-twitter": ["KanColle_STAFF", "C2_STAFF", "ywwuyi", "FlatIsNice"],
+    "pcr-twitter": ["priconne_redive", "priconne_anime"],
+    "uma-twitter": ["uma_musu", "uma_musu_anime"],
+    "pripri-twitter": ["pripri_anime"],
+    "coffee-favorite-twitter": ["shiratamacaron", "k_yuizaki", "suzukitoto0323", "usagicandy_taku", "usagi_takumichi"],
+    "depress-artist-twitter": ["tkmiz"],
+    "moe-artist-twitter": [
+        "koma_momozu", "santamatsuri", "panno_mimi", "suimya", "Anmi_", "mamgon",
+        "kazukiadumi", "Setmen_uU", "bakuPA", "kantoku_5th", "done_kanda", "hoshi_u3",
+        "siragagaga", "fuzichoco", "miyu_miyasaka", "naco_miyasaka", "tsukimi08",
+        "tsubakininiwawa", "_Dan_ball", "ominaeshin", "gomalio_y", "izumiyuhina",
+        "1kurusk", "amsrntk3", "kani_biimu", "Nakkar7", "li_hongbo", "nahaki_401",
+        "ukiukisoda", "yukkieeeeeen", "t_takahashi0830", "riko0202", "enoki_art",
+        "Zoirun", "rulu_py", "zo3mie",
+    ],
+}
+
+media_only_users = [
+    *follows["moe-artist-twitter"],
+    *follows["depress-artist-twitter"],
+]
