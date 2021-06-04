8c3ee735213c7b18e1f4cb4c8514d494b274ab64
Ice-Cirno <438971718@qq.com>
2021-06-04 09:17:59 +0800

    allow `bc` in public  session

--- hoshino/modules/botmanage/broadcast.py
+++ hoshino/modules/botmanage/broadcast.py
@@ -5,21 +5,25 @@ from hoshino.service import sucmd
 from hoshino.typing import CommandSession, CQHttpError
 
 
-@sucmd('broadcast', aliases=('bc', '广播'))
+@sucmd('broadcast', aliases=('bc', '广播'), force_private=False)
 async def broadcast(session: CommandSession):
     msg = session.current_arg
+    bot = session.bot
+    ev = session.event
+    su = session.event.user_id
     for sid in hoshino.get_self_ids():
-        gl = await session.bot.get_group_list(self_id=sid)
-        gl = [ g['group_id'] for g in gl ]
+        gl = await bot.get_group_list(self_id=sid)
+        gl = [g['group_id'] for g in gl]
+        await bot.send_private_msg(self_id=sid, user_id=su, message=f"开始向{len(gl)}个群广播：\n{msg}")
         for g in gl:
             await asyncio.sleep(0.5)
             try:
-                await session.bot.send_group_msg(self_id=sid, group_id=g, message=msg)
+                await bot.send_group_msg(self_id=sid, group_id=g, message=msg)
                 hoshino.logger.info(f'群{g} 投递广播成功')
-            except Exception as e:
+            except CQHttpError as e:
                 hoshino.logger.error(f'群{g} 投递广播失败：{type(e)}')
                 try:
-                    await session.send(f'群{g} 投递广播失败：{type(e)}')
+                    await bot.send_private_msg(self_id=sid, user_id=su, message=f'群{g} 投递广播失败：{type(e)}')
                 except Exception as e:
                     hoshino.logger.critical(f'向广播发起者进行错误回报时发生错误：{type(e)}')
-    await session.send(f'广播完成！')
+    await bot.send_private_msg(self_id=ev.self_id, user_id=su, message='广播完成！')
