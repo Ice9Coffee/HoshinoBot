c7dfb24bfb5c76a4cc864979d8871742f9486eae
lulu <794329728@qq.com>
2021-06-18 17:06:11 +0800

    fix

--- hoshino/config/__bot__.py
+++ hoshino/config/__bot__.py
@@ -22,6 +22,8 @@ COMMAND_SEP = set()  # 命令分隔符（hoshino不需要该特性，保持为se
 RES_PROTOCOL = 'file'
 # 资源库文件夹，需可读可写，windows下注意反斜杠转义
 RES_DIR = r'./res/'
+# 数据库文件夹
+DB_DIR = r'./data/'
 # 使用http协议时需填写，原则上该url应指向RES_DIR目录
 RES_URL = 'http://127.0.0.1:5000/static/'
 
