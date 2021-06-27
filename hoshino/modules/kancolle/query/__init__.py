9c1a53a452b05a8bd5396a54a19a52574e7dfa67
Ice-Cirno <438971718@qq.com>
2021-06-27 16:56:36 +0800

    kancolle query: add new entries

--- hoshino/modules/kancolle/query/__init__.py
+++ hoshino/modules/kancolle/query/__init__.py
@@ -4,11 +4,11 @@ from hoshino import R
 sv_help = '''
 [人事表200102] 战果人事表(数字为 年/月/服务器)
 [.qj 晓] 预测运值增加(准确率高达25%)(需开启dice)
-[dd2] 驱逐改二特殊装备早见表
-[远征] 远征大成功条件表
 [e5攻略] 直达梦美妈妈攻略贴
 [*晓改二] 舰娘信息查询(暂不可用)
 [*震电] 装备信息查询(暂不可用)
+=== 速查表 ===
+[dd2][远征][蜜瓜]
 '''.strip()
 sv = Service('kc-query', enable_on_default=False, help_=sv_help, bundle='kancolle')
 
@@ -20,6 +20,8 @@ sv.on_fullmatch('e5攻略', 'E5攻略')(lambda bot, ev: bot.send(ev, 'bbs.nga.cn
 
 sv.on_fullmatch('驱逐改二', 'dd改二', 'DD改二', 'dd2')(lambda bot, ev: bot.send(ev, R.img('kancolle/quick/驱逐改二早见表.jpg').cqcode))
 sv.on_fullmatch('远征')(lambda bot, ev: bot.send(ev, f"bbs.nga.cn/read.php?tid=21866416 {R.img('kancolle/quick/远征大成功.png').cqcode}"))
+sv.on_fullmatch('蜜瓜', '夕张')(lambda bot, ev: bot.send(ev, f"https://zh.kcwiki.cn/wiki/%E5%A4%95%E5%BC%A0#.E6.88.98.E6.96.97.E7.89.B9.E6.80.A7 {R.img('kancolle/quick/夕张改二装备适性.png').cqcode}"))
+
 
 from .fleet import *
 from .senka import *
