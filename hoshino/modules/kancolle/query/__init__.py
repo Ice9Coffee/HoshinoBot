4a822c9431d075c6aaafabfa08befbb34965481b
Ice-Cirno <438971718@qq.com>
2021-09-04 00:31:36 +0800

    data update

--- hoshino/modules/kancolle/query/__init__.py
+++ hoshino/modules/kancolle/query/__init__.py
@@ -12,11 +12,19 @@ sv_help = '''
 '''.strip()
 sv = Service('kc-query', enable_on_default=False, help_=sv_help, bundle='kancolle')
 
-sv.on_fullmatch('e1攻略', 'E1攻略')(lambda bot, ev: bot.send(ev, 'bbs.nga.cn/read.php?pid=514129578'))
-sv.on_fullmatch('e2攻略', 'E2攻略')(lambda bot, ev: bot.send(ev, 'bbs.nga.cn/read.php?pid=514129640'))
-sv.on_fullmatch('e3攻略', 'E3攻略')(lambda bot, ev: bot.send(ev, 'bbs.nga.cn/read.php?pid=514129692'))
-sv.on_fullmatch('e4攻略', 'E4攻略')(lambda bot, ev: bot.send(ev, 'bbs.nga.cn/read.php?pid=514129759'))
-sv.on_fullmatch('e5攻略', 'E5攻略')(lambda bot, ev: bot.send(ev, 'bbs.nga.cn/read.php?pid=514129810'))
+sv.on_fullmatch('拆包')(lambda bot, ev: bot.send(ev, 'bbs.nga.cn/read.php?tid=28148427'))
+temp = '''极简信息搬运贴bbs.nga.cn/read.php?tid=28130801
+与Верный酱的地中海假日bbs.nga.cn/read.php?tid=28149658
+记者的舰娘笔记bbs.nga.cn/read.php?tid=28154058
+目白麦昆开荒组bbs.nga.cn/read.php?tid=28142296
+地中海就是波罗的海，马耳他就是奥兰bbs.nga.cn/read.php?tid=28142877
+ぜかましzekamashi.net/category/202108-event/
+'''
+sv.on_fullmatch('e1攻略', 'E1攻略')(lambda bot, ev: bot.send(ev, temp))
+sv.on_fullmatch('e2攻略', 'E2攻略')(lambda bot, ev: bot.send(ev, temp))
+sv.on_fullmatch('e3攻略', 'E3攻略')(lambda bot, ev: bot.send(ev, temp))
+# sv.on_fullmatch('e4攻略', 'E4攻略')(lambda bot, ev: bot.send(ev, temp))
+# sv.on_fullmatch('e5攻略', 'E5攻略')(lambda bot, ev: bot.send(ev, temp))
 
 sv.on_fullmatch('驱逐改二', 'dd改二', 'DD改二', 'dd2')(lambda bot, ev: bot.send(ev, R.img('kancolle/quick/驱逐改二早见表.jpg').cqcode))
 sv.on_fullmatch('远征')(lambda bot, ev: bot.send(ev, f"bbs.nga.cn/read.php?tid=21866416 {R.img('kancolle/quick/远征大成功.png').cqcode} {R.img('kancolle/quick/远征大成功简.png').cqcode}"))
