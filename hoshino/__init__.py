af0bef8fdbcbd7efdd8bd3d623504f26003eecda
Ice-Cirno <438971718@qq.com>
2021-09-06 20:00:19 +0800

    fix import bug

--- hoshino/__init__.py
+++ hoshino/__init__.py
@@ -4,13 +4,14 @@ import nonebot
 from nonebot.message import CanceledException
 from nonebot import Message, MessageSegment, message_preprocessor
 
+HoshinoBot = nonebot.NoneBot
+
 from . import log, config
 from .service import Service, sucmd
 
 __version__ = '2.2.0'
 
 _bot = None
-HoshinoBot = nonebot.NoneBot
 os.makedirs(os.path.expanduser('~/.hoshino'), exist_ok=True)
 logger = log.new_logger('hoshino', config.DEBUG)
 
@@ -51,5 +52,3 @@ def get_self_ids():
     if _bot is None:
         raise ValueError('HoshinoBot has not been initialized')
     return _bot._wsr_api_clients.keys()
-
-
